6e05dc068bb7aed369cfc7bc164cddc2ec3c4060
Snowghost24 <Snowghost@Joses-MacBook-Air.local>
2017-12-28 10:47:10 -0500

    first commit

---
+++ app.jsx
@@ -0,0 +1,13 @@
+function Application (){
+  return (
+    <div className="application">
+    <div clasName="header">
+    <h1> ScoreBoard</h1>
+    </div>
+    
+      <p>I'm rendering a react component</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<Application/>, document.getElementById('container'));
