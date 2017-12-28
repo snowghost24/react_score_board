d581b57343eb2bcb7f9813ae4c3abb883bc0a2e2
Snowghost24 <Snowghost@Joses-MacBook-Air.local>
2017-12-28 12:00:33 -0500

    added header component

--- app.jsx
+++ app.jsx
@@ -1,13 +1,52 @@
-function Application (){
+function Header (props){
+  return(
+    <div className="header">
+    <h1>{props.title}</h1>
+  </div>
+  )
+}
+
+Header.propTypes ={
+  title:React.PropTypes.string.isRequired
+  }
+
+
+function Application(props) {
   return (
-    <div className="application">
-    <div clasName="header">
-    <h1> ScoreBoard</h1>
-    </div>
-    
-      <p>I'm rendering a react component</p>
+    <div className="scoreboard">
+     <Header title={props.title}/>
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            Jose Guzman
+        </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement">-</button>
+              <div className="counter-score">31</div>
+              <button className="counter-action increment">+</button>
+            </div>
+          </div>
+        </div>
+        <div className="player">
+          <div className="player-name">
+            Michael Jordan
+        </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement">-</button>
+              <div className="counter-score">31</div>
+              <button className="counter-action increment">+</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-ReactDOM.render(<Application/>, document.getElementById('container'));
+Application.propTypes ={
+title:React.PropTypes.string.isRequired
+}
+
+ReactDOM.render(<Application title={"My scoreboard"} />, document.getElementById('container'));
