12619034c06a544f8413f56c29696a9ee673b7fe
Jose Guzman <jguzman24680@gmail.com>
2017-12-30 23:37:16 -0500

    added increment score
    fixed decrement score

--- app.jsx
+++ app.jsx
@@ -53,9 +53,9 @@ var Counter = React.createClass({
   render: function(){
     return(
       <div className="counter">
-      <button className="counter-action decrement onClick={this.decrementScore}>-</button>
+      <button className="counter-action decrement" onClick={this.decrementScore}>-</button>
       <div className="counter-score">{this.state.score}</div>
-      <button className="counter-action increment">+</button>
+      <button className="counter-action increment" onClick={this.incrementScore}>+</button>
     </div> 
     )
   }
